8279fc47bc357e8e2360efe144f9bf4d58725a4e
Theo Pack Test <tf.pack@gmail.com>
2016-08-30 20:42:22 +0200

    Use own written proxy

---
+++ proxy/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var host = process.env.PROXY_HOST || 'ping-pong-manager';
+var port = process.env.PROXY_PORT || 8080;
+
+http.createServer(onRequest).listen(3000);
+
+function onRequest(client_req, client_res) {
+  console.log('serve: ' + client_req.url);
+
+  var options = {
+    hostname: host,
+    port: port,
+    path: client_req.url,
+    method: 'GET'
+  };
+
+  var proxy = http.request(options, function (res) {
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
